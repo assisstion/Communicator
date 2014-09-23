d4f716f54f6c14860b49894924d8c99ac5110ab5
Markus Feng <zzz1112222595@hotmail.com>
2014-09-23 00:05:49 -0400

    Minor fixes

---
+++ src/com/github/assisstion/Communicator/relay/BSocketHandlerImpl.java
@@ -0,0 +1,58 @@
+package com.github.assisstion.Communicator.relay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class BSocketHandlerImpl extends BSocketHandlerImplAbstract<String>{
+
+	protected PrintWriter out;
+	protected BufferedReader in;
+
+	public BSocketHandlerImpl(BSocketProcessor<String> processor){
+		super(processor);
+	}
+
+	public BSocketHandlerImpl(Socket socket, BSocketProcessor<String> processor){
+		super(socket, processor);
+	}
+
+	@Override
+	protected void initialize() throws IOException{
+
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(
+				new InputStreamReader(socket.getInputStream()));
+	}
+
+	@Override
+	public void readFromIn() throws IOException{
+		String inputLine;
+
+		while (!closed && (inputLine = in.readLine()) != null) {
+			if(!closed){
+				try{
+					if(!processor.isInputBlockingEnabled()){
+						new Thread(new Inputtor(inputLine)).start();
+					}
+					else{
+						new Inputtor(inputLine).run();
+					}
+				}
+				catch(Exception e){
+					if(!closed){
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void writeToOut(String out) throws IOException{
+		this.out.println(out);
+	}
+
+}
