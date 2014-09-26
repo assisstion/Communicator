69b6348f59d5cac219dba7de8b1a1c10ac970990
Markus Feng <zzz1112222595@hotmail.com>
2014-09-26 13:22:42 -0400

    Minor changes

--- src/com/github/assisstion/Communicator/relay/ClientMachineSocket.java
+++ src/com/github/assisstion/Communicator/relay/ClientMachineSocket.java
@@ -3,7 +3,7 @@ package com.github.assisstion.Communicator.relay;
 import java.net.Socket;
 
 /**
- * A Socket wrapped in a MachineSocket.
+ * A Socket wrapped in a MachineSocket. Use get() to retrieve the Socket.
  *
  * @author Markus Feng
  */
