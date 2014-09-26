e1b4144e8af3c06d2eeab4588481e49186710d64
Markus Feng <zzz1112222595@hotmail.com>
2014-09-26 13:24:42 -0400

    Updated comments

--- src/com/github/assisstion/Communicator/relay/SocketMachine.java
+++ src/com/github/assisstion/Communicator/relay/SocketMachine.java
@@ -1,18 +1,3 @@
-/**
- * The com.github.assisstion.Communicator.relay API contains classes to
- * enable programmers to easily connect from one Socket to another and to
- * provide custom functionality associated with the sockets. Each package
- * that is labeled with a letter of the alphabet is a stand-alone package
- * that depends only on packages with a letter closer to the beginning of
- * the alphabet, which all eventually depend on this package. Each package
- * later in the alphabet provide higher-level programming functionality
- * than the previous, allowing programmers to choose feasibility, flexibility,
- * or a mixture of both when working with the API. This API is still being
- * updated, so not all interfaces are in their final form. Feel free to
- * extend any class to provide your own additional functionality.
- *
- * @author Markus Feng
- */
 package com.github.assisstion.Communicator.relay;
 
 import java.io.Closeable;
